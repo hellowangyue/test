e55d94efb782c68a8973f4f07701f817c2a26e37
hellowangyue <18845117946>
2019-12-03 15:59:42 +0800

    2019/12/3

---
+++ src/main/java/com/mengxuegu/springboot01hello/SpringBoot01HelloApplication.java
@@ -0,0 +1,13 @@
+package com.mengxuegu.springboot01hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot01HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot01HelloApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/mengxuegu/springboot01hello/controller/helloController.java
@@ -0,0 +1,14 @@
+package com.mengxuegu.springboot01hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class helloController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return  "hello";
+    }
+}
